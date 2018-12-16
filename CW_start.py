83dc351d80e847a14baa18dcb535b1ea65e43611
Josh Boult <sc15jlb@leeds.ac.uk>
2018-12-16 13:20:11 +0000

    Working AI

--- CW_start.py
+++ CW_start.py
@@ -18,11 +18,11 @@ def running_mean(x, N=20):
 
 class Agent:
     def __init__(self, env):
-        self.stateCnt      = self.env.observation_space.n
-        self.actionCnt     = self.env.action_space.n # left:0; down:1; right:2; up:3
-        self.learning_rate = 0.1
+        self.stateCnt      = env.observation_space.n
+        self.actionCnt     = env.action_space.n # left:0; down:1; right:2; up:3
+        self.learning_rate = 0.5
         self.gamma         = 0.9
-        self.epsilon       = 0.1
+        self.epsilon       = 0.8
         self.Q             = self._initialiseModel()
 
     def _initialiseModel(self):
@@ -30,7 +30,7 @@ class Agent:
         # Q should be an array of structure:
         # [[a1, a2, a3, a4], -- State 0 ([0][0])
         #  [a1, a2, a3, a4], -- State 1 ([0][1]) etc
-        Q = np.zeros((self.stateCnt,self.actionCnt))
+        Q = np.zeros((self.stateCnt,self.actionCnt)) + 1
 
         return Q
 
@@ -42,32 +42,32 @@ class Agent:
 
     def update_value_Qlearning(self, s,a,r,s_next, terminalStateNotReached):
         # Update if s is not a terminal state
-        if terminalStateNotReached:
-            self.Q[s][a] = self.Q[s][a] + self.learning_rate*(r - self.Q[s][a] + self.gamma*np.amax(self.Q[s_next]))
+        if terminalStateNotReached == True:
+            self.Q[s][a] += self.learning_rate*(r - self.Q[s][a] + self.gamma*np.amax(self.Q[s_next]))
 
         # Update if s is any type of terminal state
         else:
-            self.Q[s][a] = self.Q[s][a] + self.learning_rate*(r - self.Q[s][a])
+            self.Q[s][a] += self.learning_rate*(r - self.Q[s][a])
 
 
     def update_value_SARSA(self, s,a,r,s_next, a_next, terminalStateNotReached):
         # Update if s is not a terminal state
-        if terminalStateNotReached:
-            self.Q[s][a] = self.Q[s][a] + self.learning_rate*(r - self.Q[s][a] + self.gamma*self.Q[s_next][a_next])
+        if terminalStateNotReached == True:
+            self.Q[s][a] += self.learning_rate*(r - self.Q[s][a] + self.gamma*self.Q[s_next][a_next])
 
         # Update if s is any type of terminal state
         else:
-            self.Q[s][a] = self.Q[s][a] + self.learning_rate*(r - self.Q[s][a])
+            self.Q[s][a] += self.learning_rate*(r - self.Q[s][a])
 
 
     def choose_action(self, s):
         # Random float between 0 and 1
         # If greater than epsilon value, choose optimal action
         if random.random() > self.epsilon:
-            a = np.amax(self.Q[s])
+            a = np.argmax(self.Q[s])
         # If less than or equal to epsilon, choose any random action (inc. optimal)
         else:
-            a = random.randrange(0,4,1)
+            a = random.randrange(0,self.actionCnt,1)
         return a
 
 
@@ -80,8 +80,8 @@ class World:
         print('Environment has %d states and %d actions.' % (self.env.observation_space.n, self.env.action_space.n))
         self.stateCnt           = self.env.observation_space.n
         self.actionCnt          = self.env.action_space.n
-        self.maxStepsPerEpisode = 20
-        self.q_Sinit_progress   = agent.Q[0] # ex: np.array([[0,0,0,0]])
+        self.maxStepsPerEpisode = 1000
+        self.q_Sinit_progress   = np.array([[1,1,1,1]]) # All initial actions have reward 0 at start state
 
     def run_episode_qlearning(self):
         s               = self.env.reset() # "reset" environment to start state
@@ -94,7 +94,7 @@ class World:
             # step will return the next state, the reward, a boolean indicating if a terminal state is reached, and some diagnostic information useful for debugging.
             s_prev = s
             a = agent.choose_action(s)
-            s, r, done = self.env.step(a)
+            s, r, done, diag = self.env.step(a)
 
             # Update value function
             agent.update_value_Qlearning(s_prev,a,r,s,not done)
@@ -102,6 +102,7 @@ class World:
             # Print the current environment state
             self.env.render()
 
+            print(done)
             # Break if terminal state reached
             if done == True:
                 # Record success if goal reached
@@ -112,7 +113,7 @@ class World:
             r_total += r
             episodeStepsCnt += 1
         # self.q_Sinit_progress = np.append( ): use q_Sinit_progress for monitoring the q value progress throughout training episodes for all available actions at the initial state.
-        self.q_Sinit_progress = np.append(agent.predict_value(0))
+        self.q_Sinit_progress = np.append(self.q_Sinit_progress, [agent.predict_value(0)], axis=0)
 
         return r_total, episodeStepsCnt, success
 
@@ -126,7 +127,7 @@ class World:
             # Take step to the next states
             s_prev = s
             a = agent.choose_action(s)
-            s, r, done = self.env.step(a)
+            s, r, done, diag = self.env.step(a)
             a_next = agent.choose_action(s)
 
             # Update value function
@@ -145,7 +146,7 @@ class World:
             r_total += r
             episodeStepsCnt += 1
 
-        self.q_Sinit_progress = np.append(agent.predict_value(0))
+        self.q_Sinit_progress = np.append(self.q_Sinit_progress, [agent.predict_value(0)], axis=0)
 
         return r_total, episodeStepsCnt, success
 
@@ -156,7 +157,7 @@ class World:
 
         for i in range(self.maxStepsPerEpisode):
             # Move to next state
-            s, r, done = self.env.step(agent.choose_action(s))
+            s, r, done, diag = self.env.step(agent.choose_action(s))
 
             # Render
             self.env.render()
@@ -175,15 +176,20 @@ if __name__ == '__main__':
     agent                    = Agent(env) # This will creat an agent
     r_total_progress         = []
     episodeStepsCnt_progress = []
-    nbOfTrainingEpisodes     = 50
+    nbOfTrainingEpisodes     = 2000
     for i in range(nbOfTrainingEpisodes):
-        print '\n========================\n   Episode: {}\n========================'.format(i)
+        print('\n========================\n   Episode: {}\n========================'.format(i))
         r_total, episodeStepsCnt, success = world.run_episode_qlearning()
         # r_total, episodeStepsCnt = world.run_episode_sarsa()
         if success == True:
-            r_total_progress = np.append(r_total)
-            episodeStepsCnt_progress = np.append(episodeStepsCnt)
-    world.run_evaluation_episode()
+            r_total_progress = np.append(r_total_progress, [r_total], axis=0)
+            episodeStepsCnt_progress = np.append(episodeStepsCnt_progress, [episodeStepsCnt], axis=0)
+    success = world.run_evaluation_episode()
+    print(success)
+
+    print(r_total_progress)
+    print(episodeStepsCnt_progress)
+    print(world.q_Sinit_progress)
 
     ### --- Plots --- ###
     # 1) plot world.q_Sinit_progress
